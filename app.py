34ff9ce7ff92f347cb34140da28c271badec1cdc
mikias <mikiyasabate003@gmail.com>
2024-01-07 22:53:38 +0300

    i have added

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
+@app.route('/')
+@app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('home  this is the code .html')
 
 @app.route('/market')
 def market_page():
