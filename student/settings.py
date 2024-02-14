7b283952af00e1b4a0829f4832f669165d8254e5
mikias <mikiyasabate003@gmail.com>
2024-02-14 17:05:02 +0300

    added

--- student/settings.py
+++ student/settings.py
@@ -77,18 +77,16 @@ WSGI_APPLICATION = "student.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'student_management_system',
-        'USER':'student_management_system',
-        'PASSWORD':'student_management_password',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'miki1219',
+        'HOST': 'localhost',  # Or your PostgreSQL server address
+        'PORT': '1219',        # Or your PostgreSQL server port
     }
 }
 
-
+LOGIN_URL = 'doLogin' 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -123,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
